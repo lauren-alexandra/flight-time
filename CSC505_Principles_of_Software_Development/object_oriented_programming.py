146f054fd4c99fadec2151efd3eefe0589e98cee
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-04-07 15:43:21 -0700

    init 505

---
+++ CSC505_Principles_of_Software_Development/object_oriented_programming.py
@@ -0,0 +1,17 @@
+"""
+OOP is a programming paradigm that provides a means of structuring
+programs so that properties and behaviors are bundled into individual
+objects.
+
+An object contains data and behavior.
+"""
+
+
+
+
+
+
+
+
+
+
