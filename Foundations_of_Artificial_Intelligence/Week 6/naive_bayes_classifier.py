4716a7c663cb2c3ac06e2fd048d2bf2579146ae2
Lauren Alexandra <lauren@laurenalexandra.dev>
2022-01-24 14:51:13 -0800

    na bayes

--- Foundations_of_Artificial_Intelligence/Week 6/naive_bayes_classifier.py
+++ Foundations_of_Artificial_Intelligence/Week 6/naive_bayes_classifier.py
@@ -13,11 +13,12 @@ cost=['under-5','under-20','under-10','under-30','under-30','under-5','under-20'
 read=['Yes','Yes','No','Yes','Yes','Yes','No','No','Yes','No','Yes','Yes','Yes','Yes','Yes','Yes','No','Yes','Yes','Yes','No','No','Yes','No','Yes','Yes','Yes','Yes']
 
 output = {
-    1: "Get this book!",
+    1: "Buy this book!",
     0: "Don't buy this book."
 }
 
-ro_no, ro_yes, th_no, th_yes, my_no, my_yes, his_no, his_yes, under_five_no, under_five_yes, under_ten_no, under_ten_yes, under_twenty_no, under_twenty_yes, under_thirty_no, under_thirty_yes = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+# default is 1. Laplacian correction applied later.
+ro_no, ro_yes, th_no, th_yes, my_no, my_yes, his_no, his_yes, under_five_no, under_five_yes, under_ten_no, under_ten_yes, under_twenty_no, under_twenty_yes, under_thirty_no, under_thirty_yes = (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 
 # collect frequency counts 
 genre_zip = list(zip(genre, read))
@@ -54,18 +55,28 @@ features = np.transpose((genre, cost))
 # split into train and test sets 
 X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3,random_state=32)
 
-# apply Laplacian correction by adding 1 data point for each feature-label pair
+# apply Laplacian correction by adding 1 data point for each feature where class probability 0
 """
-[0, 3, 1]  
-[1, 2, 0]
-[2, 1, 0]
-[3, 0, 1]
+thriller no
+history no
+under-5 no
+under-30 no
+under-10 yes 
+
+2 3 1 0 - 'Romance','Thriller','Mystery','History'
+3 1 0 2 - 'under-5','under-20','under-10','under-30'
+1 0 - 'Yes', 'No'
+
+[0, 3, 0]  
+[2, 2, 0]
+[3, 0, 0]
+[0, 0, 1]
 """
 
 # add to label
-lab_add = [1, 0, 0, 1]
+lab_add = [0, 0, 0, 1]
 # add to features
-feat_add = [[0, 3], [1, 2], [2, 1], [3, 0]]
+feat_add = [[0, 3], [2, 2], [3, 0], [0, 0]]
 
 X_train = np.concatenate((X_train, feat_add), axis=0)
 y_train = np.concatenate((y_train, lab_add), axis=0)
@@ -78,24 +89,24 @@ model.fit(X_train, y_train)
 # predict 
 book1 = model.predict([[2,3]]) # 2:Romance, 3:under-5
 book1prob = model.predict_proba([[2,3]])
-print("Given a book under $5 in the Romance genre...")
+print("\nGiven a book under $5 in the Romance genre...")
 print("Predicted Value:", book1[0])
 print(output[book1[0]])
 
 book2 = model.predict([[2,0]]) # 2:Romance, 0:under-10
-print("Given a book under $10 in the Romance genre...")
+print("\nGiven a book under $10 in the Romance genre...")
 print("Predicted Value:", book2[0])
 print(output[book2[0]])
 
 book3 = model.predict([[0,2]]) # 0:History, 2:under-30
-print("Given a book under $30 in the History genre...")
+print("\nGiven a book under $30 in the History genre...")
 print("Predicted Value:", book3[0])
 print(output[book3[0]])
 
 book4 = model.predict([[1,0]]) # 1:Mystery, 2:under-10
-print("Given a book under $10 in the Mystery genre...")
+print("\nGiven a book under $10 in the Mystery genre...")
 print("Predicted Value:", book4[0])
-print(output[book4[0]])
+print(output[book4[0]], "\n")
 
 # Calculate the posterior probability by converting the dataset into a frequency table.
 
@@ -108,8 +119,6 @@ freq_table = pd.DataFrame(list(zip(no_vals, yes_vals)), index=feat_list, columns
 no_total = freq_table['No'].sum()
 yes_total = freq_table['Yes'].sum()
 freq_table.loc['Total'] = [no_total, yes_total]
-print("Frequency Table")
-print(freq_table)
 
 # Likelihood table for the posterior probability
 post_prob_no = []
@@ -127,5 +136,5 @@ post_prob_yes.append('')
 freq_table['Posterior Probability for No'] = post_prob_no
 freq_table['Posterior Probability for Yes'] = post_prob_yes
   
-print("Posterior Probability for each class")
+# Posterior Probability for each class
 print(freq_table)
