fd954b30e3e10a9b2ff43be489ed686c5d232aca
Lauren Alexandra <lauren@laurenalexandra.dev>
2022-05-03 22:16:15 -0700

    frames

--- Foundations_of_Computer_Vision/Week 3/drawing.py
+++ Foundations_of_Computer_Vision/Week 3/drawing.py
@@ -5,8 +5,8 @@ import cv2 as cv
 img = cv.imread('profile.jpg')
 
 parser = argparse.ArgumentParser(description='Face and eye cascades')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='\\data\\haarcascades\\haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='\\data\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='data\\haarcascades\\haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
 args = parser.parse_args()
 face_cascade_name = args.face_cascade
 eyes_cascade_name = args.eyes_cascade
@@ -28,17 +28,19 @@ def detectAndDisplay(frame):
     faces = face_cascade.detectMultiScale(frame_gray)
     for (x,y,w,h) in faces:
         center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (128, 255, 0), 4)
         faceROI = frame_gray[y:y+h,x:x+w]
-        cv.putText(img, str('this is me'), (x+5,y-5), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv.putText(img, str('this is me'), (x+70,y-10), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         # in each face, detect eyes
         eyes = eyes_cascade.detectMultiScale(faceROI)
+
+        roi_color = img[y:y+h, x:x+w]
         for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    cv.imshow('Face and eyes detected', frame)
-    k = cv.waitKey(4000)
+            frame = cv.rectangle(roi_color, (x2,y2),(x2+w2,y2+h2),(0,0,255),2)
+
+    cv.imshow('Face and eyes detected', img)
+    k = cv.waitKey(3000)
 
 
-detectAndDisplay(img)
+detectAndDisplay(img)    
+cv.imwrite("face_detected.png", img)
