5a9e2eaf9650e4c1aca34c14aa8168bd803a94b9
Lauren Alexandra <lauren@laurenalexandra.dev>
2023-01-30 10:49:19 -0800

    proj org

--- Foundations_of_Operating_Systems/Week 2/numbers.py
+++
@@ -1,9 +0,0 @@
-import random 
-
-f = open("file2.txt", "a")
-
-for i in range(1000):
-    rand = random.randint(0, 10000)
-    f.write("% s\n" % (rand))
-
-f.close()
--- Foundations_of_Operating_Systems/Week 2/numbers.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..1000}
-do
-    echo $RANDOM >> file1.txt
-done
